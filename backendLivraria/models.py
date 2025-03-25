29503e907f94f046e4c4444054bb024aae5903bf
renascm_ <renatochagas.m@gmail.com>
2025-03-25 16:13:38 -0300

    feat: started  project bookstore

---
+++ backendLivraria/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.descricao
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=255)
+    site = models.URLField(null=True, blank=True)
+    
+    def __str__(self):
+        return self_nome
+    
